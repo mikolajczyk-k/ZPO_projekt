0a1346f4b00dfcaee776bc4befc6359ec89aad70
Kacper Mikolajczyk <kacper.mikolajczyk1903@gmail.com>
2024-05-04 18:51:59 +0200

    fetchign transactions

--- nextbank_react/src/components/RecentTransactionsContainer.tsx
+++ nextbank_react/src/components/RecentTransactionsContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
@@ -8,10 +8,37 @@ interface Props {
   selectedAccountId: number | null;
 }
 
+interface Transaction {
+  id: number;
+  type: string;
+  donorId: number | null;
+  recipientId: number | null;
+  date: string;
+}
+
 const RecentTransactionsContainer: React.FC<Props> = ({
   selectedAccountId,
 }) => {
-  return <span>{selectedAccountId}</span>;
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8080/transactions/account/${selectedAccountId}?fromDate=2023-01-01`
+      )
+      .then((response) => {
+        setTransactions(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error("failed to load transactions");
+        setError(err);
+        setIsLoading(false);
+      });
+  }, []);
+  return <></>;
 };
 
 export default RecentTransactionsContainer;
